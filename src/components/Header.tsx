fbd50cb16112048d6439f3803bf4f36bbff8c4f2
Ingrid Larissa <larissaorcx@gmail.com>
2022-01-06 00:29:52 -0300

    Finalizando o componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import {Flex, Text, Input, Icon} from '@chakra-ui/react'
-import {RiSearchLine} from 'react-icons/ri'
+import {Flex, Text, Input, Icon, HStack, Box, Avatar} from '@chakra-ui/react'
+import {RiSearchLine, RiNotificationLine, RiUserAddLine} from 'react-icons/ri'
 
 export function Header() {
     return(
@@ -18,6 +18,8 @@ export function Header() {
                <Text as="span" ml="1" color="pink.500">.</Text>
             </Text>
             <Flex
+
+
               as="label"
               flex="1"
               py="4"
@@ -41,8 +43,31 @@ export function Header() {
                 />
                 
                 <Icon as={RiSearchLine} fontSize="20"/>
+            </Flex>
 
-            </Flex> 
+            <Flex
+              align="center"
+              ml="auto"
+              >
+                <HStack 
+                spacing="4"mx="8"
+                pr="8"
+                py="1"
+                color="gray.300"
+                borderRightWidth={1}
+                borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20"/>
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+                <Flex align='center'>
+                    <Box mr="4" textAlign="right">
+                        <Text>Ingrid Larissa</Text>
+                        <Text color="gray.300" fontSize="small">Larissaorcx@gmail.com</Text>
+                    </Box>
+                    <Avatar size="md" name='Ingrid Larissa' src="https://github.com/larissaorcx.png"/>
+                </Flex>
+            </Flex>
         </Flex>
     );
 }
