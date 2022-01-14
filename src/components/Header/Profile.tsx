aa88108750184ac54f0de117de077f6d039667c8
Ingrid Larissa <larissaorcx@gmail.com>
2022-01-13 21:56:16 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
     return (
         <Flex align='center'>
-            <Box mr="4" textAlign="right">
-                <Text>Ingrid Larissa</Text>
-                <Text color="gray.300" fontSize="small">Larissaorcx@gmail.com</Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Ingrid Larissa</Text>
+                    <Text color="gray.300" fontSize="small">Larissaorcx@gmail.com</Text>
+                </Box>
+            )}
             <Avatar size="md" name='Ingrid Larissa' src="https://github.com/larissaorcx.png"/>
         </Flex>
     );
